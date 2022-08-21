cc7533fca3ea2b8c76d79eb906be036c78091f03
dmiclea <dcmiclea@gmail.com>
2022-08-21 03:37:15 +0300

    Refactor

--- src/test/java/orangeHRMTests/LoginTest.java
+++ src/test/java/orangeHRMTests/LoginTest.java
@@ -7,12 +7,12 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends BaseFor {
 
-    @Parameters({"usernameValid", "passwordValid"})
+   // @Parameters({"usernameValid", "passwordValid"})
     @Test
-    public void verifyLoginWithCorrectCredentials(String username, String password) {
+    public void verifyLoginWithCorrectCredentials() {
         reportUtils.createTestCare(new Throwable().getStackTrace()[0].getMethodName());
         reportUtils.addTestLog(Status.INFO, "Performing log");
-        loginPage.login(username, password);
+        loginPage.login("admin", "admin123");
         reportUtils.addTestLog(Status.INFO, "User is logged");
         String expectedTitle = "OrangeHRM";
         String actualTitle = loginPage.getTitle();
@@ -20,22 +20,22 @@ public class LoginTest extends BaseFor {
         Assert.assertEquals(actualTitle, expectedTitle);
     }
 
-    @Parameters({"usernameInvalid", "passwordValid"})
+   // @Parameters({"usernameInvalid", "passwordValid"})
     @Test
-    public void verifyLoginWithWrongUsername(String username, String password) {
+    public void verifyLoginWithWrongUsername() {
         reportUtils.createTestCare(new Throwable().getStackTrace()[0].getMethodName());
         reportUtils.addTestLog(Status.INFO, "Performing log");
-        loginPage.login(username, password);
+        loginPage.login("username", "password");
         reportUtils.addTestLog(Status.INFO, "Comparing expected and actual title");
         Assert.assertTrue(elementControl.isDisplayed(loginPage.invalidCredentials));
     }
 
-    @Parameters({"usernameValid", "passwordInvalid"})
+    //@Parameters({"usernameValid", "passwordInvalid"})
     @Test
-    public void verifyLoginWithWrongPassword(String username, String password) {
+    public void verifyLoginWithWrongPassword() {
         reportUtils.createTestCare(new Throwable().getStackTrace()[0].getMethodName());
         reportUtils.addTestLog(Status.INFO, "Performing log");
-        loginPage.login(username, password);
+        loginPage.login("username", "password");
         reportUtils.addTestLog(Status.INFO, "Comparing expected and actual title");
         Assert.assertTrue(elementControl.isDisplayed(loginPage.invalidCredentials));
     }
